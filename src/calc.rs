d1f9527e1d40cc7d2c70bc7d9caa111ada348551
camdenboren <9UtEfABpSSrV3g.code@mailbox.org>
2025-02-01 12:31:54 -0600

    rp: cat-specific calc fn's w/ inline via round_to_place

--- src/calc.rs
+++ src/calc.rs
@@ -3,27 +3,18 @@
 
 use crate::types::{match_category, Category};
 
-fn carbonated(percentage: f32) -> f32 {
-    let raw = 1772.6 * percentage.powf(-0.996);
-    return (raw * 100.0).round() / 100.0;
-}
-
-fn hard(percentage: f32) -> f32 {
-    let raw = 3062.9 * percentage.powf(-1.161);
-    return (raw * 100.0).round() / 100.0;
-}
-
-fn liqueur(percentage: f32) -> f32 {
-    let raw = 235.94 * (percentage * -1.161).exp();
-    return (raw * 100.0).round() / 100.0;
+fn round_to_place(raw: f32, place: f32) -> f32 {
+    let base: f32 = 10.0;
+    let scaler: f32 = base.powf(place);
+    return (raw * scaler).round() / scaler;
 }
 
 pub fn alc_weight(alc_type: &str, percentage: f32) -> (u32, f32) {
     let cat: Category = match_category(alc_type);
     match cat {
-        Category::Carbonated => (1, carbonated(percentage)),
-        Category::Liqueur => (2, liqueur(percentage)),
-        Category::Hard => (3, hard(percentage)),
+        Category::Carbonated => (1, round_to_place(1772.6 * percentage.powf(-0.996), 2.0)),
+        Category::Liqueur => (2, round_to_place(235.94 * (percentage * -1.161).exp(), 2.0)),
+        Category::Hard => (3, round_to_place(3062.9 * percentage.powf(-1.161), 2.0)),
     }
 }
 
@@ -36,5 +27,11 @@ mod tests {
         let (num, weight) = alc_weight("Hard", 40.0);
         assert_eq!(num, 3);
         assert_eq!(weight, 42.28);
+    }
+
+    #[test]
+    fn test_round_to_place() {
+        let num = round_to_place(42.281, 2.0);
+        assert_eq!(num, 42.28);
     }
 }
