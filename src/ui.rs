7e9e30cb5816be4310e43b993574c9acfe8a5519
camdenboren <9UtEfABpSSrV3g.code@mailbox.org>
2025-02-02 18:53:50 -0600

    mv: centered-ish text to child, rm: rgb

--- src/ui.rs
+++ src/ui.rs
@@ -13,9 +13,9 @@ pub struct UI {
 
 impl UI {
     pub fn new(cx: &mut WindowContext) -> View<Self> {
-        let (numm, _weight) = alc_weight("Hard", 40.0);
+        let (numm, _weight) = alc_weight("Liqueur", 40.0);
         cx.new_view(|_cx| UI {
-            text: "World".into(),
+            text: "calc".into(),
             num: numm,
         })
     }
@@ -25,28 +25,21 @@ impl Render for UI {
     fn render(&mut self, _cx: &mut ViewContext<Self>) -> impl IntoElement {
         div()
             .font_family(".SystemUIFont")
-            .flex()
-            .flex_col()
-            .gap_3()
             .bg(rgb(0x505050))
-            .items_center()
             .size_full()
             .shadow_lg()
             .text_xl()
             .text_color(rgb(0xffffff))
             .child(if OS == "linux" { titlebar() } else { div() })
-            .child(format!("Hello, {}, {}!", &self.text, &self.num))
             .child(
                 div()
                     .flex()
-                    .gap_2()
+                    .flex_col()
+                    .gap_3()
+                    .size_full()
                     .justify_center()
-                    .child(div().size_8().bg(gpui::red()))
-                    .child(div().size_8().bg(gpui::green()))
-                    .child(div().size_8().bg(gpui::blue()))
-                    .child(div().size_8().bg(gpui::yellow()))
-                    .child(div().size_8().bg(gpui::black()))
-                    .child(div().size_8().bg(gpui::white())),
+                    .items_center()
+                    .child(format!("alc-{} {}", &self.text, &self.num)),
             )
     }
 }
