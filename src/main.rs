31dced9f26c8e787ba250ce21e670a020e546faa
camdenboren <9UtEfABpSSrV3g.code@mailbox.org>
2025-02-05 17:16:04 -0600

    feature: quit keybind

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,20 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 
 use alc_calc::ui::UI;
-use gpui::{App, AppContext, WindowOptions};
+use gpui::{actions, App, AppContext, KeyBinding, Menu, MenuItem, WindowOptions};
+
+actions!(alc_alc, [Quit]);
 
 fn main() {
     App::new().run(|cx: &mut AppContext| {
+        cx.activate(true);
+        cx.on_action(|_: &Quit, cx| cx.quit());
+        cx.bind_keys([KeyBinding::new("ctrl-q", Quit, None)]);
+        cx.set_menus(vec![Menu {
+            name: "alc-calc".into(),
+            items: vec![MenuItem::action("Quit", Quit)],
+        }]);
+
         cx.open_window(
             WindowOptions {
                 focus: true,
