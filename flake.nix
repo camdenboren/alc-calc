fdfb4806ea9a9c6bd93cf87c2a722d0a2bf5d8cf
camdenboren <9UtEfABpSSrV3g.code@mailbox.org>
2025-03-06 16:38:36 -0600

    Revert "ci: rm fixDarwinDylibNames and configure cargo-bundle"
    This reverts commit eb754e68233b7acbd488056f6964613ef0129281.

--- flake.nix
+++ flake.nix
@@ -47,7 +47,6 @@
               bashInteractive
               rustc
               cargo
-              cargo-bundle
               rust-analyzer
               rustfmt
               build
@@ -80,33 +79,8 @@
             cargoHash = "sha256-9kfU14JiOd2cItjXwGc2OtpztDnqns4AIewWvd5M4pg=";
             useFetchCargoVendor = true;
             buildInputs = deps;
-            nativeBuildInputs =
-              with pkgs;
-              lib.optionals stdenv.hostPlatform.isDarwin [
-                pkg-config
-                cargo-bundle
-              ];
+            nativeBuildInputs = with pkgs; lib.optionals stdenv.hostPlatform.isDarwin [ fixDarwinDylibNames ];
             buildFeatures = with pkgs; lib.optionals stdenv.hostPlatform.isDarwin [ "gpui/runtime_shaders" ];
-            env.LIBCLANG_PATH =
-              with pkgs;
-              lib.optionalString stdenv.hostPlatform.isDarwin "${lib.getLib llvmPackages.libclang}/lib";
-
-            installPhase =
-              with pkgs;
-              ''
-                runHook preInstall
-                release_target="target/${stdenv.hostPlatform.rust.cargoShortTarget}/release"
-              ''
-              + lib.optionalString stdenv.hostPlatform.isDarwin ''
-                mv $release_target/alc-calc target/release/alc-calc
-                export CARGO_BUNDLE_SKIP_BUILD=true
-                app_path=$(cargo bundle --release | xargs)
-                mkdir -p $out/Applications $out/bin
-                mv $app_path $out/Applications/
-              ''
-              + ''
-                runHook postInstall
-              '';
 
             postFixup =
               with pkgs;
@@ -114,6 +88,9 @@
                 patchelf --add-rpath ${wayland}/lib $out/bin/*
                 patchelf --add-rpath ${vulkan-loader}/lib $out/bin/*
               '';
+            env.LIBCLANG_PATH =
+              with pkgs;
+              lib.optionalString stdenv.hostPlatform.isDarwin "${lib.getLib llvmPackages.libclang}/lib";
 
             meta = {
               description = "";
