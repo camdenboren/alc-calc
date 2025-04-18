32071da9fd36bba56ac1b3d900548d8931ddd095
camdenboren <9UtEfABpSSrV3g.code@mailbox.org>
2025-04-18 09:08:57 -0500

    ui: zoom in/out window by double-clicking titlebar

--- src/ui/titlebar.rs
+++ src/ui/titlebar.rs
@@ -6,12 +6,18 @@ use gpui::{div, prelude::*, px};
 
 pub fn titlebar() -> impl IntoElement {
     div()
+        .id("titlebar")
         .flex()
         .h(px(32.))
         .items_center()
         .px_4()
         .bg(gpui::opaque_grey(0.2, 1.0))
         .w_full()
+        .on_click(|event, window, _| {
+            if event.up.click_count == 2 {
+                window.zoom_window();
+            }
+        })
         .child(div().flex().items_center().justify_center().size_full())
         .child(button("", "close.svg", |_, window, _| {
             window.remove_window();
