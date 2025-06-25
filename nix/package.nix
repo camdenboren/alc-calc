a2d62524caf64bd6495cbf7e25bd94b45b673e73
camdenboren <9UtEfABpSSrV3g.code@mailbox.org>
2025-06-25 08:20:12 -0500

    bundle: add linux installPhase and .desktop file

--- nix/package.nix
+++ nix/package.nix
@@ -42,6 +42,13 @@
         mv target/release/alc-calc $out/bin/
         mv $app_path $out/Applications/
       ''
+      + pkgs.lib.optionalString pkgs.stdenv.hostPlatform.isLinux ''
+        install -Dm755 $release_target/alc-calc $out/bin/alc-calc
+
+        install -Dm644 $src/img/brand/app-icon@2x.png $out/share/icons/hicolor/1024x1024@2x/apps/alc-calc.png
+        install -Dm644 $src/img/brand/app-icon.png $out/share/icons/hicolor/512x512/apps/alc-calc.png
+        install -Dm644 $src/os/linux/alc-calc.desktop $out/share/applications/alc-calc.desktop
+      ''
       + ''
         runHook postInstall
       '';
