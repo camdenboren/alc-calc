camdenboren/alc-calc

commit 5e7811de42fc57f60e8b1dd2a36a695979dcab76

ui: reimplement `cursor_state` and consolidate `Tab, TabPrev`
The cursor is now visible whenever a new input is focused via `Tab,
TabPrev, Toggle`, which was made more ergonomic by consolidated the
separate `Tab, TabPrev` implementations in `UI, Table` -> `UI` alone via
subscribing to `Table`’s `Add {}` event (this required some changes to
the tests in `UI` that use `Tab`). Some additional improvements have
also been made:
- `ThemeMenu` now hides on `Tab, TabPrev`.
- `Dropdown` now scrolls to the focused item when shown on `Tab,
TabPrev`

src/ui.rs
@@ -19,8 +19,8 @@ use crate::ui::{
     view::{menu::ThemeMenu, table::data_table::Table, titlebar::Titlebar},
 };
 use gpui::{
-    App, ClipboardItem, Entity, FocusHandle, Focusable, Global, KeyBinding, PromptLevel,
-    SharedString, Window, actions, deferred, div, prelude::*,
+    App, ClipboardItem, Entity, EventEmitter, FocusHandle, Focusable, Global, KeyBinding,
+    PromptLevel, SharedString, Window, actions, deferred, div, prelude::*,
 };
 
 actions!(
@@ -110,12 +110,52 @@ impl UI {
         #[cfg(not(test))]
         Theme::set(cx);
 
+        let table = cx.new(|cx| Table::new(window, cx));
+        cx.subscribe(&table, |this: &mut UI, _table, _event, cx| this.on_add(cx))
+            .detach();
+
         UI {
             menu: cx.new(ThemeMenu::new),
-            table: cx.new(|cx| Table::new(window, cx)),
+            table,
             titlebar: cx.new(|_| Titlebar::default()),
             focus_handle: cx.focus_handle().tab_index(0).tab_stop(false),
         }
+    }
+
+    /// Subscribe menu and table.num_drinks_input to UI's events.
+    /// Both subscribe to Tab, TabPrev, but only num_drinks_input subscribes to Toggle atm
+    pub fn init(&mut self, cx: &mut Context<Self>) {
+        cx.subscribe_self(|this: &mut UI, Tab, cx| {
+            this.menu.update(cx, |menu, cx| menu.hide(cx));
+            this.table
+                .update(cx, |table, cx| table.show_num_drinks_cursor(cx))
+        })
+        .detach();
+        cx.subscribe_self(|this: &mut UI, TabPrev, cx| {
+            this.menu.update(cx, |menu, cx| menu.hide(cx));
+            this.table
+                .update(cx, |table, cx| table.show_num_drinks_cursor(cx))
+        })
+        .detach();
+        cx.subscribe_self(|this, Toggle, cx| {
+            this.table
+                .update(cx, |table, cx| table.show_num_drinks_cursor(cx))
+        })
+        .detach();
+    }
+
+    /// Subscribe new ingreds to UI's Tab, TabPrev events
+    fn on_add(&mut self, cx: &mut Context<Self>) {
+        cx.subscribe_self(|this: &mut UI, Tab, cx| {
+            this.table
+                .update(cx, |table, cx| table.show_cursor_and_hide_dd(cx));
+        })
+        .detach();
+        cx.subscribe_self(|this: &mut UI, TabPrev, cx| {
+            this.table
+                .update(cx, |table, cx| table.show_cursor_and_hide_dd(cx));
+        })
+        .detach();
     }
 
     fn quit(&mut self, _: &Quit, _window: &mut Window, cx: &mut Context<Self>) {
@@ -184,16 +224,23 @@ impl UI {
             }
             self.table.read(cx).num_drinks_input.read(cx).focus(window);
         }
+        cx.emit(Toggle {});
     }
 
-    fn on_tab(&mut self, _: &Tab, window: &mut Window, _: &mut Context<Self>) {
+    fn on_tab(&mut self, _: &Tab, window: &mut Window, cx: &mut Context<Self>) {
         window.focus_next();
+        cx.emit(Tab {});
     }
 
-    fn on_tab_prev(&mut self, _: &TabPrev, window: &mut Window, _: &mut Context<Self>) {
+    fn on_tab_prev(&mut self, _: &TabPrev, window: &mut Window, cx: &mut Context<Self>) {
         window.focus_prev();
+        cx.emit(TabPrev {});
     }
 }
+
+impl EventEmitter<Tab> for UI {}
+impl EventEmitter<TabPrev> for UI {}
+impl EventEmitter<Toggle> for UI {}
 
 impl Render for UI {
     fn render(&mut self, window: &mut Window, cx: &mut Context<Self>) -> impl IntoElement {
@@ -301,7 +348,7 @@ mod tests {
         assert_eq!(true, table_focused)
     }
 
-    fn setup_ui(cx: &mut TestAppContext) -> (Entity<UI>, &mut VisualTestContext, SharedString) {
+    pub fn setup_ui(cx: &mut TestAppContext) -> (Entity<UI>, &mut VisualTestContext, SharedString) {
         Theme::test(cx);
         let mut ctrl: SharedString = "".into();
         cx.update(|cx| {

src/ui/comp/dropdown.rs
@@ -114,6 +114,7 @@ impl Dropdown {
     fn show(&mut self, _: &Enter, _window: &mut Window, cx: &mut Context<Self>) {
         self.show = true;
         self.prev = Some(self.current.clone());
+        self.scroll();
         cx.notify();
     }
 

src/ui/comp/input/cursor_state.rs
@@ -1,80 +1,101 @@
 // SPDX-FileCopyrightText: Camden Boren
 // SPDX-License-Identifier: GPL-3.0-or-later
 
-// Adapted from: https://github.com/lumehq/coop/blob/master/crates/ui/src/input/blink_cursor.rs
+// Adapted from: https://github.com/zed-industries/zed/blob/main/crates/editor/src/blink_manager.rs
 
 use gpui::{Context, Timer};
 use std::time::Duration;
 
 const INTERVAL: u64 = 500;
-const DELAY: u64 = 300;
 
 pub struct CursorState {
-    show: bool,
-    paused: bool,
-    epoch: usize,
+    blink_epoch: usize,
+    blinking_paused: bool,
+    visible: bool,
+    enabled: bool,
+}
+
+impl Default for CursorState {
+    fn default() -> Self {
+        Self::new()
+    }
 }
 
 impl CursorState {
     pub fn new() -> Self {
         Self {
-            show: false,
-            paused: false,
-            epoch: 0,
+            blink_epoch: 0,
+            blinking_paused: false,
+            visible: true,
+            enabled: false,
         }
     }
 
-    pub fn start(&mut self, cx: &mut Context<Self>) {
-        self.blink(self.epoch, cx);
+    fn next_blink_epoch(&mut self) -> usize {
+        self.blink_epoch += 1;
+        self.blink_epoch
     }
 
-    pub fn stop(&mut self, cx: &mut Context<Self>) {
-        self.epoch = 0;
-        cx.notify();
+    pub fn pause_blinking(&mut self, cx: &mut Context<Self>) {
+        self.show_cursor(cx);
+
+        let epoch = self.next_blink_epoch();
+        cx.spawn(async move |this, cx| {
+            Timer::after(Duration::from_millis(INTERVAL)).await;
+            this.update(cx, |this, cx| this.resume_cursor_blinking(epoch, cx))
+        })
+        .detach();
     }
 
-    fn next_epoch(&mut self) -> usize {
-        self.epoch += 1;
-        self.epoch
+    fn resume_cursor_blinking(&mut self, epoch: usize, cx: &mut Context<Self>) {
+        if epoch == self.blink_epoch {
+            self.blinking_paused = false;
+            self.blink_cursors(epoch, cx);
+        }
     }
 
-    fn blink(&mut self, epoch: usize, cx: &mut Context<Self>) {
-        if self.paused || epoch != self.epoch {
+    fn blink_cursors(&mut self, epoch: usize, cx: &mut Context<Self>) {
+        if epoch == self.blink_epoch && self.enabled && !self.blinking_paused {
+            self.visible = !self.visible;
+            cx.notify();
+
+            let epoch = self.next_blink_epoch();
+            cx.spawn(async move |this, cx| {
+                Timer::after(Duration::from_millis(INTERVAL)).await;
+                if let Some(this) = this.upgrade() {
+                    this.update(cx, |this, cx| this.blink_cursors(epoch, cx))
+                        .ok();
+                }
+            })
+            .detach();
+        }
+    }
+
+    pub fn show_cursor(&mut self, cx: &mut Context<Self>) {
+        if !self.visible {
+            self.visible = true;
+            cx.notify();
+        }
+    }
+
+    pub fn enable(&mut self, cx: &mut Context<Self>) {
+        if self.enabled {
             return;
         }
 
-        self.show = !self.show;
-        let epoch = self.next_epoch();
-        cx.notify();
-
-        cx.spawn(async move |this, cx| {
-            Timer::after(Duration::from_millis(INTERVAL)).await;
-            if let Some(this) = this.upgrade() {
-                this.update(cx, |this, cx| this.blink(epoch, cx)).ok();
-            }
-        })
-        .detach();
+        self.enabled = true;
+        // Set cursors as invisible and start blinking: this causes cursors
+        // to be visible during the next render.
+        self.visible = false;
+        self.blink_cursors(self.blink_epoch, cx);
     }
 
-    pub fn show(&self) -> bool {
-        self.paused || self.show
+    pub fn disable(&mut self, _cx: &mut Context<Self>) {
+        self.visible = false;
+        self.enabled = false;
     }
 
-    pub fn pause(&mut self, cx: &mut Context<Self>) {
-        self.paused = true;
-        let epoch = self.next_epoch();
-        cx.notify();
-
-        cx.spawn(async move |this, cx| {
-            Timer::after(Duration::from_millis(DELAY)).await;
-            if let Some(this) = this.upgrade() {
-                this.update(cx, |this, cx| {
-                    this.paused = false;
-                    this.blink(epoch, cx);
-                })
-                .ok();
-            }
-        })
-        .detach();
+    pub fn visible(&self) -> bool {
+        self.visible
     }
 }

src/ui/comp/input/text_element.rs
@@ -120,7 +120,7 @@ impl Element for TextElement {
             (
                 None,
                 // conditionally show cursor based on blink state
-                if self.input.read(cx).show_cursor(window, cx) {
+                if self.input.read(cx).should_show_cursor(window, cx) {
                     Some(fill(
                         Bounds::new(
                             point(bounds.left() + cursor_pos, bounds.top()),

src/ui/comp/input/text_input.rs
@@ -44,7 +44,7 @@ enum InputEvent {
 const CONTEXT: &str = "TextInput";
 
 pub struct TextInput {
-    cursor_state: Entity<CursorState>,
+    pub cursor_state: Entity<CursorState>,
     pub focus_handle: FocusHandle,
     pub content: SharedString,
     pub placeholder: SharedString,
@@ -87,25 +87,12 @@ impl TextInput {
         ]);
 
         let focus_handle = cx.focus_handle().tab_index(tab_index).tab_stop(true);
-        let cursor_state = cx.new(|_| CursorState::new());
-        let _subscriptions = vec![
-            cx.observe(&cursor_state, |_, _, cx| cx.notify()),
-            cx.observe_window_activation(window, |input, window, cx| {
-                if window.is_window_active() {
-                    let focus_handle = input.focus_handle.clone();
-                    if focus_handle.is_focused(window) {
-                        input.cursor_state.update(cx, |cursor_state, cx| {
-                            cursor_state.start(cx);
-                        });
-                    }
-                }
-            }),
-            cx.on_focus(&focus_handle, window, Self::on_focus),
-            cx.on_blur(&focus_handle, window, Self::on_blur),
-        ];
+        cx.on_focus(&focus_handle, window, Self::on_focus).detach();
+        cx.on_blur(&focus_handle, window, Self::on_blur).detach();
+        let cursor_state = cx.new(|_| CursorState::default());
 
         Self {
-            cursor_state,
+            cursor_state: cursor_state.clone(),
             focus_handle,
             content: "".into(),
             placeholder,
@@ -116,7 +103,21 @@ impl TextInput {
             last_layout: None,
             last_bounds: None,
             is_selecting: false,
-            _subscriptions,
+            _subscriptions: vec![
+                cx.observe(&cursor_state, |_, _, cx| cx.notify()),
+                cx.observe_window_activation(window, |input, window, cx| {
+                    if window.is_window_active() {
+                        let active = window.is_window_active();
+                        input.cursor_state.update(cx, |blink_manager, cx| {
+                            if active {
+                                blink_manager.enable(cx);
+                            } else {
+                                blink_manager.disable(cx);
+                            }
+                        });
+                    }
+                }),
+            ],
         }
     }
 
@@ -130,26 +131,31 @@ impl TextInput {
 
     fn on_focus(&mut self, _: &mut Window, cx: &mut Context<Self>) {
         self.cursor_state.update(cx, |cursor, cx| {
-            cursor.start(cx);
+            cursor.enable(cx);
         });
         cx.emit(InputEvent::Focus);
     }
 
     fn on_blur(&mut self, _: &mut Window, cx: &mut Context<Self>) {
         self.cursor_state.update(cx, |cursor, cx| {
-            cursor.stop(cx);
+            cursor.disable(cx);
         });
         cx.emit(InputEvent::Blur);
     }
 
     fn pause_blink(&mut self, cx: &mut Context<Self>) {
         self.cursor_state.update(cx, |cursor, cx| {
-            cursor.pause(cx);
+            cursor.pause_blinking(cx);
         });
     }
 
-    pub fn show_cursor(&self, window: &mut Window, cx: &App) -> bool {
-        self.is_focused(window) && self.cursor_state.read(cx).show()
+    pub fn show_cursor(&self, cx: &mut Context<Self>) {
+        self.cursor_state
+            .update(cx, |cursor, cx| cursor.show_cursor(cx));
+    }
+
+    pub fn should_show_cursor(&self, window: &mut Window, cx: &App) -> bool {
+        self.is_focused(window) && self.cursor_state.read(cx).visible()
     }
 
     fn left(&mut self, _: &Left, _: &mut Window, cx: &mut Context<Self>) {
@@ -324,6 +330,7 @@ impl TextInput {
     fn move_to(&mut self, offset: usize, cx: &mut Context<Self>) {
         self.selected_range = offset..offset;
         self.pause_blink(cx);
+
         cx.notify()
     }
 

src/ui/util/window.rs
@@ -20,7 +20,9 @@ const SHADOW_SIZE: Pixels = px(12.0);
 
 pub fn new_window(cx: &mut App) {
     if let Ok(_window) = cx.open_window(window_options(cx), |window, cx| {
-        cx.new(|cx| UI::new(window, cx))
+        let ui = cx.new(|cx| UI::new(window, cx));
+        ui.update(cx, |ui, cx| ui.init(cx));
+        ui
     }) {
     } else {
         eprintln!("alc-calc failed to open a window");

src/ui/view/menu.rs
@@ -87,6 +87,11 @@ impl ThemeMenu {
         }
     }
 
+    pub fn hide(&mut self, cx: &mut Context<Self>) {
+        self.show = false;
+        cx.notify();
+    }
+
     pub fn escape(&mut self, window: &mut Window, cx: &mut Context<Self>) {
         self.escape_key(&Escape, window, cx);
     }

src/ui/view/table/data_table.rs
@@ -24,13 +24,13 @@ use gpui::{
     div, prelude::*, px,
 };
 
-actions!(table, [Tab, TabPrev, Add, Delete, Escape, RemoveKey]);
+actions!(table, [Add, Delete, Escape, RemoveKey]);
 
 pub const CONTEXT: &str = "Table";
 pub const MAX_ITEMS: usize = 10;
 
 pub struct Table {
-    ingreds: Vec<Entity<Ingredient>>,
+    pub ingreds: Vec<Entity<Ingredient>>,
     pub num_drinks_input: Entity<TextInput>,
     num_drinks: f32,
     count: usize,
@@ -42,8 +42,6 @@ impl Table {
     pub fn new(window: &mut Window, cx: &mut Context<Self>) -> Self {
         let ctrl = cx.ctrl();
         cx.bind_keys([
-            KeyBinding::new("tab", Tab, Some(CONTEXT)),
-            KeyBinding::new("shift-tab", TabPrev, Some(CONTEXT)),
             KeyBinding::new(&format!("{ctrl}-i"), Add, Some(CONTEXT)),
             KeyBinding::new(&format!("{ctrl}-d"), Delete, Some(CONTEXT)),
             KeyBinding::new(&format!("{ctrl}-r"), RemoveKey, Some(CONTEXT)),
@@ -77,28 +75,8 @@ impl Table {
             self.ingreds.push(ingred);
             self.count += 1;
 
-            // hide dropdown on Tab, TabPrev
-            // we have to iterate through all elements anytime we append for this to work
-            cx.subscribe_self(|this: &mut Table, Tab, cx| {
-                this.ingreds.iter().for_each(|ingred| {
-                    ingred.update(cx, |ingred, cx| {
-                        ingred
-                            .ingred_type
-                            .update(cx, |ingred_type, cx| ingred_type.hide(cx))
-                    });
-                });
-            })
-            .detach();
-            cx.subscribe_self(|this: &mut Table, TabPrev, cx| {
-                this.ingreds.iter().for_each(|ingred| {
-                    ingred.update(cx, |ingred, cx| {
-                        ingred
-                            .ingred_type
-                            .update(cx, |ingred_type, cx| ingred_type.hide(cx))
-                    });
-                });
-            })
-            .detach();
+            // instruct UI to subscribe new ingreds to its Tab, TabPrev events
+            cx.emit(Add {});
         }
         cx.notify();
     }
@@ -150,6 +128,17 @@ impl Table {
             }
         }
         cx.notify();
+    }
+
+    pub fn show_num_drinks_cursor(&mut self, cx: &mut Context<Self>) {
+        self.num_drinks_input
+            .update(cx, |num_drinks, cx| num_drinks.show_cursor(cx));
+    }
+
+    pub fn show_cursor_and_hide_dd(&mut self, cx: &mut Context<Self>) {
+        self.ingreds
+            .iter()
+            .for_each(|ingred| ingred.update(cx, |ingred, cx| ingred.show_cursor_and_hide_dd(cx)));
     }
 
     fn ready(&mut self, cx: &mut Context<Self>) -> bool {
@@ -236,20 +225,9 @@ impl Table {
     fn focus(&mut self, _: &Escape, window: &mut Window, _cx: &mut Context<Self>) {
         self.focus_handle.focus(window);
     }
-
-    fn on_tab(&mut self, _: &Tab, window: &mut Window, cx: &mut Context<Self>) {
-        window.focus_next();
-        cx.emit(Tab {});
-    }
-
-    fn on_tab_prev(&mut self, _: &TabPrev, window: &mut Window, cx: &mut Context<Self>) {
-        window.focus_prev();
-        cx.emit(TabPrev {});
-    }
 }
 
-impl EventEmitter<Tab> for Table {}
-impl EventEmitter<TabPrev> for Table {}
+impl EventEmitter<Add> for Table {}
 
 impl Render for Table {
     fn render(&mut self, window: &mut Window, cx: &mut Context<Self>) -> impl IntoElement {
@@ -268,8 +246,6 @@ impl Render for Table {
 
         div()
             .key_context(CONTEXT)
-            .on_action(cx.listener(Self::on_tab))
-            .on_action(cx.listener(Self::on_tab_prev))
             .on_action(cx.listener(Self::focus))
             .on_action(cx.listener(Self::add))
             .on_action(cx.listener(Self::delete))
@@ -392,7 +368,7 @@ impl Focusable for Table {
 
 #[cfg(test)]
 mod tests {
-    use crate::ui::{ActiveCtrl, Ctrl, util::theme::Theme};
+    use crate::ui::{ActiveCtrl, Ctrl, UI, util::theme::Theme};
 
     use super::*;
     use gpui::{TestAppContext, VisualTestContext};
@@ -436,25 +412,30 @@ mod tests {
 
     #[gpui::test]
     fn test_table_remove_key_when_empty(cx: &mut TestAppContext) {
-        let (table, cx, ctrl) = setup_table(cx);
+        let (ui, cx, ctrl) = setup_ui_and_table(cx);
         let mut num_ingreds = 0;
 
-        cx.focus(&table);
+        cx.focus(&ui);
         cx.simulate_keystrokes(&format!("tab tab {ctrl}-r {ctrl}-r"));
-        table.update(cx, |table, _cx| num_ingreds = table.ingreds.len());
+        ui.update(cx, |ui, cx| {
+            ui.table
+                .update(cx, |table, _cx| num_ingreds = table.ingreds.len());
+        });
 
         assert_eq!(0, num_ingreds);
     }
 
     #[gpui::test]
     fn test_table_calc_single_ingred(cx: &mut TestAppContext) {
-        let (table, cx, _ctrl) = setup_table(cx);
+        let (ui, cx, _ctrl) = setup_ui_and_table(cx);
         let mut weight = SharedString::from("");
 
-        cx.focus(&table);
+        cx.focus(&ui);
         cx.simulate_keystrokes("tab 2 tab tab 4 0");
-        table.update(cx, |table, cx| {
-            weight = table.ingreds[0].read(cx).weight.clone()
+        ui.update(cx, |ui, cx| {
+            ui.table.update(cx, |table, cx| {
+                weight = table.ingreds[0].read(cx).weight.clone();
+            });
         });
 
         assert_eq!(SharedString::from("84.6"), weight);
@@ -462,15 +443,17 @@ mod tests {
 
     #[gpui::test]
     fn test_table_calc_multiple_ingreds(cx: &mut TestAppContext) {
-        let (table, cx, ctrl) = setup_table(cx);
+        let (ui, cx, ctrl) = setup_ui_and_table(cx);
         let mut weight: Vec<SharedString> = vec!["".into(), "".into()];
 
-        cx.focus(&table);
-        cx.simulate_keystrokes(&format!("{ctrl}-i tab 2 tab tab 4 0 tab 1 . 5"));
+        cx.focus(&ui);
+        cx.simulate_keystrokes(&format!("tab {ctrl}-i 2 tab tab 4 0 tab 1 . 5"));
         cx.simulate_keystrokes("tab enter k k k k enter tab 1 6 . 5 tab 1");
-        table.update(cx, |table, cx| {
-            weight[0] = table.ingreds[0].read(cx).weight.clone();
-            weight[1] = table.ingreds[1].read(cx).weight.clone();
+        ui.update(cx, |ui, cx| {
+            ui.table.update(cx, |table, cx| {
+                weight[0] = table.ingreds[0].read(cx).weight.clone();
+                weight[1] = table.ingreds[1].read(cx).weight.clone();
+            });
         });
 
         assert_eq!(SharedString::from("66.3"), weight[0]);
@@ -491,21 +474,42 @@ mod tests {
 
     #[gpui::test]
     fn test_table_focus_next_ingred(cx: &mut TestAppContext) {
-        let (table, cx, ctrl) = setup_table(cx);
+        let (ui, cx, ctrl) = setup_ui_and_table(cx);
         let mut ingred_focused = false;
 
-        cx.focus(&table);
+        cx.focus(&ui);
         cx.simulate_keystrokes(&format!("tab tab {ctrl}-i"));
         (0..3).for_each(|_| cx.simulate_keystrokes(&format!("tab")));
-        table.update_in(cx, |table, window, cx| {
-            ingred_focused = table.ingreds[1]
-                .read(cx)
-                .ingred_type
-                .read(cx)
-                .is_focused(window)
+        ui.update_in(cx, |ui, window, cx| {
+            ui.table.update(cx, |table, cx| {
+                ingred_focused = table.ingreds[1]
+                    .read(cx)
+                    .ingred_type
+                    .read(cx)
+                    .is_focused(window)
+            });
         });
 
         assert_eq!(true, ingred_focused);
+    }
+
+    fn setup_ui_and_table(
+        cx: &mut TestAppContext,
+    ) -> (Entity<UI>, &mut VisualTestContext, SharedString) {
+        Theme::test(cx);
+        let mut ctrl: SharedString = "".into();
+        cx.update(|cx| {
+            Ctrl::set(cx);
+            ctrl = cx.ctrl();
+        });
+
+        let (ui, cx) = cx.add_window_view(|window, cx| UI::new(window, cx));
+        ui.update_in(cx, |ui, window, cx| {
+            ui.init(cx);
+            ui.table = cx.new(|cx| Table::new(window, cx));
+        });
+
+        (ui, cx, ctrl)
     }
 
     fn setup_table(

commit 59aa87d1223521d9d63281e30a153f15368f325b

ui: fix memory leak caused by adding new subscriptions in `ui.on_add()`
The approach implemented in 5e7811d introduced a subtle memory leak
since detaching a `subscribe_self()` implies it will persist until UI
itself is dropped, rather than the ingredient, so adding and removing
large numbers of ingredients would cause an infinitely increasing number
of UI subscriptions.

This resolves the issue by popping the dedicated ingreds subscriptions
before appending updated ones in `on_add()`, which was made possible by
using the dedicated `subscriptions` field from bff1338b

src/ui.rs
@@ -52,7 +52,7 @@ pub struct UI {
     table: Entity<Table>,
     titlebar: Entity<Titlebar>,
     focus_handle: FocusHandle,
-    _subscriptions: Vec<Subscription>,
+    subscriptions: Vec<Subscription>,
 }
 
 impl UI {
@@ -65,10 +65,10 @@ impl UI {
     /// - Keybinds
     /// - Menus (on macOS)
     /// - Subscriptions
-    ///   - menu + table both sub to Tab, TabPrev
-    ///   - table also subs to Toggle
+    ///   - menu + table.num_drinks_input both sub to Tab, TabPrev
+    ///   - table.num_drinks_input also subs to Toggle
     ///
-    /// *Note that the UI is notified of new ingredients to create subscriptions for
+    /// *Note that the UI is notified of new ingredients to recreate subscriptions for
     /// by subscribing to Table's Add event*
     pub fn new(window: &mut Window, cx: &mut Context<Self>) -> Self {
         Toast::set(cx);
@@ -112,7 +112,7 @@ impl UI {
             table,
             titlebar: cx.new(|_| Titlebar::default()),
             focus_handle: cx.focus_handle().tab_index(0).tab_stop(false),
-            _subscriptions: vec![
+            subscriptions: vec![
                 cx.subscribe_self(|this: &mut UI, Tab, cx| {
                     this.menu.update(cx, |menu, cx| menu.hide(cx));
                     this.table
@@ -131,18 +131,22 @@ impl UI {
         }
     }
 
-    /// Subscribe new ingreds to UI's Tab, TabPrev events
+    /// Refresh ingreds' subscriptions to Tab, TabPrev events when a new one is added
     fn on_add(&mut self, cx: &mut Context<Self>) {
-        cx.subscribe_self(|this: &mut UI, Tab, cx| {
-            this.table
-                .update(cx, |table, cx| table.show_cursor_and_hide_dd(cx));
-        })
-        .detach();
-        cx.subscribe_self(|this: &mut UI, TabPrev, cx| {
-            this.table
-                .update(cx, |table, cx| table.show_cursor_and_hide_dd(cx));
-        })
-        .detach();
+        while self.subscriptions.len() > 3 {
+            self.subscriptions.pop();
+        }
+
+        self.subscriptions.append(&mut vec![
+            cx.subscribe_self(|this: &mut UI, Tab, cx| {
+                this.table
+                    .update(cx, |table, cx| table.show_cursor_and_hide_dd(cx));
+            }),
+            cx.subscribe_self(|this: &mut UI, TabPrev, cx| {
+                this.table
+                    .update(cx, |table, cx| table.show_cursor_and_hide_dd(cx));
+            }),
+        ]);
     }
 
     fn quit(&mut self, _: &Quit, _window: &mut Window, cx: &mut Context<Self>) {